26d9f2c9d2d2b8d4533149aaf0740fa57e9c5774
Jisley <kerlly.castillo.pastrana@utelvt.edu.ec>
2022-09-23 20:06:49 +0000

    comparación

---
+++ comparacion.cpp
@@ -0,0 +1,21 @@
+//Creado por: Kerlly Castillo
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"ingrese a b:"; cin>>a>>b;
+	if(a>b)
+	{
+	  cout<<a<<" es mayor a "<<b<<endl;
+
+
+	}else{
+
+	  cout<<b<<" es mayor a "<<a<<endl;
+
+
+	}
+
+	return 0;
+}
